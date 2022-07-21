d10ec58e15c79f9a80fddad240f15516d2b6d721
Paulo Barbosa <74267828+paulo-barbosa2006@users.noreply.github.com>
2022-07-21 17:27:56 +0100

    .

--- main.js
+++ main.js
@@ -8,27 +8,25 @@ function sequência(){
     let res = document.querySelector('#res')
 
     if(contador1.value.length >= 0 && contador2.value.length >= 0 && seq.value.length >= 0){
-    res.innerHTML = 'contando... <br>'
-    let c1 = Number(contador1.value)
-    let c2 = Number(contador2.value)
-    let s = Number(seq.value)
+        res.innerHTML = 'contando... <br>'
+        let c1 = Number(contador1.value)
+        let c2 = Number(contador2.value)
+        let s = Number(seq.value)
 
-    if(s == 0){
-        alert('Passo 0 é inválido. Considerando Passo 1')
-        s = 1
-    }
+        if(s == 0){
+            alert('Passo 0 é inválido. Considerando Passo 1')
+            s = 1
+        }
 
-    if(c1 < c2){
-        // Contagem crescente
-        for(let c = c1; c <= c2; c += s){
-            res.innerHTML += `${c} \u{1F449}`
+        if(c1 < c2){
+            for(let c = c1; c <= c2; c += s){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }else{
+            for(let c = c1; c1 >= c2; c1 -= s){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-    }else{
-        // Contagem decrescente
-        for(let c = c1; c >= c2; c -= s){
-            res.innerHTML += `${c} \u{1F449}`
-        }
-    }
-    res.innerHTML += `\u{1F3C1}` 
+        res.innerHTML += `\u{1F3C1}`
     }
 }
